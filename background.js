40d17197150b5d5fbfa062b916d86079724985b4
wb_huyunan <wb_huyunan@kuaishou.com>
2025-09-16 19:59:36 +0800

    重新设计定时提醒

--- background.js
+++ background.js
@@ -1,64 +1,66 @@
 // 时间工具函数
 const TimeUtils = {
-    getMilliseconds: (hours, minutes, seconds) => {
-        minutes += hours * 60
-        seconds += minutes * 60
-        return seconds * 1000
-    },
-    parseMilliseconds: (milliseconds) => {
-        const SECOND = 1000
-        const MINUTE = 60 * SECOND
-        const HOUR = 60 * MINUTE
-        const hours = padZero(Math.floor(milliseconds/ HOUR))
-        const minutes = padZero(Math.floor((milliseconds % HOUR) / MINUTE))
-        const seconds = padZero(Math.floor((milliseconds % MINUTE) / SECOND))
-        return `${hours}:${minutes}:${seconds}`
-    },
+  getMilliseconds: (hours, minutes, seconds) => {
+    minutes += hours * 60;
+    seconds += minutes * 60;
+    return seconds * 1000;
+  },
+  parseMilliseconds: (milliseconds) => {
+    const SECOND = 1000;
+    const MINUTE = 60 * SECOND;
+    const HOUR = 60 * MINUTE;
+    const hours = padZero(Math.floor(milliseconds / HOUR));
+    const minutes = padZero(Math.floor((milliseconds % HOUR) / MINUTE));
+    const seconds = padZero(Math.floor((milliseconds % MINUTE) / SECOND));
+    return `${hours}:${minutes}:${seconds}`;
+  },
 };
 
 // 补0，如1 -> 01
 function padZero(num, targetLength = 2) {
-    let str = `${num}`
-    while (str.length < targetLength) {
-        str = `0${str}`
-    }
-    return str
+  let str = `${num}`;
+  while (str.length < targetLength) {
+    str = `0${str}`;
+  }
+  return str;
 }
 
 // UI 工具函数
-function showNotification(message) {
-    chrome.notifications.clear('定时提醒Event')
-    // 以后在设置页设置这些
-    // eventTime 与通知关联的时间戳 Date.now() + n
-    // requireInteraction 通知应一直显示在屏幕上，直到用户激活或关闭通知
-    // silent 静音 目前不好使，不能出声音，可能和电脑配置有关
-    chrome.notifications.create('定时提醒Event', {
-        type: 'basic',
-        iconUrl: 'icons/icon128.png',
-        requireInteraction: true,
-        title: message,
-        message: ''
-    });
+function showNotification(message, page) {
+  chrome.notifications.clear("定时提醒Event" + page);
+  // 以后在设置页设置这些
+  // eventTime 与通知关联的时间戳 Date.now() + n
+  // requireInteraction 通知应一直显示在屏幕上，直到用户激活或关闭通知
+  // silent 静音 目前不好使，不能出声音，可能和电脑配置有关
+  chrome.notifications.create("定时提醒Event" + page, {
+    type: "basic",
+    iconUrl: "icons/icon128.png",
+    requireInteraction: true,
+    title: message,
+    message: "",
+  });
 }
 
 // 事件检查
-async function checkNextEvent() {
-    const nextEvent = await timeManager.getNextEvent()
-    if (nextEvent && nextEvent.open) {
-        await timeManager.startForNextEvent(nextEvent)
-    }
+async function checkNextEvent(page) {
+  const pageEvent = await timeManager.getPageEvent(page);
+  if (pageEvent && pageEvent.open) {
+    await timeManager.startForPageEvent(pageEvent, page);
+  }
 }
 
 // 初始化扩展
-let flag = true
+let flag = true;
 async function initializeExtension() {
-    if (flag) {
-        flag = false
-        await checkNextEvent();
-        setTimeout(() => {
-            flag = true
-        }, 1000 * 30);
-    }
+  if (flag) {
+    flag = false;
+    await checkNextEvent("page1");
+    await checkNextEvent("page2");
+    await checkNextEvent("page3");
+    setTimeout(() => {
+      flag = true;
+    }, 1000 * 30);
+  }
 }
 
 // 监听浏览器启动
@@ -67,229 +69,176 @@ chrome.runtime.onStartup.addListener(initializeExtension);
 // 监听扩展安装/更新
 chrome.runtime.onInstalled.addListener(initializeExtension);
 
-chrome.windows.onCreated.addListener(initializeExtension)
+chrome.windows.onCreated.addListener(initializeExtension);
 // 监听来自 popup 的消息
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    if (message.type === 'TIME_START') {
-        await timeManager.start(message.id)
-        return true
-    } else if (message.type === 'TIME_PAUSE') {
-        await timeManager.pause(message.id)
-        return true
-    } else if (message.type === 'ADD_EVENT') {
-        await timeManager.addEvent(message.name, message.date)
-        chrome.runtime.sendMessage({ type: 'ADD_SUCCESS' });
-        return true
-    } else if (message.type === 'DELETE_EVENT') {
-        await timeManager.deleteEvent(message.id)
-        return true
-    }
+  if (message.type === "TIME_START") {
+    await timeManager.start(message.page);
+    return true;
+  } else if (message.type === "TIME_PAUSE") {
+    await timeManager.pause(message.page);
+    return true;
+  } else if (message.type === "ADD_EVENT") {
+    await timeManager.addEvent(message.name, message.date, message.page);
+    chrome.runtime.sendMessage({ type: "ADD_SUCCESS", page: message.page });
+    return true;
+  } else if (message.type === "DELETE_EVENT") {
+    await timeManager.deleteEvent(message.page);
+    return true;
+  }
 });
 // 事件管理
 const timeManager = {
-    async getEvents() {
-        const result = await chrome.storage.local.get(['events']);
-        return result.events || [];
-    },
-    
-    async getNextEvent() {
-        const result = await chrome.storage.local.get(['nextEvent']);
-        return result.nextEvent;
-    },
-    
-    async getEndTime() {
-        const result = await chrome.storage.local.get(['endTime']);
-        return result.endTime || 0;
-    },
-    
-    async getTimer() {
-        const result = await chrome.storage.local.get(['timer']);
-        return result.timer;
-    },
-    
-    async saveEvents(events) {
-        await chrome.storage.local.set({ events });
-    },
-    
-    async saveNextEvent(nextEvent) {
-        await chrome.storage.local.set({ nextEvent });
-    },
-    
-    async saveEndTime(endTime) {
-        await chrome.storage.local.set({ endTime });
-    },
-    
-    async saveTimer(timer) {
-        await chrome.storage.local.set({ timer });
-    },
-    
-    async addEvent(name, date) {
-        if (!name) {
-            alert('请填写定时事件名称！');
-            return;
-        }
+  async getTabPage() {
+    const result = await chrome.storage.local.get(["tabPage"]);
+    return result.tabPage || "page1";
+  },
 
-        if (!date || date === '00:00:00') {
-            alert('请填写定时事件时间！');
-            return;
+  async getPageEvent(page) {
+    const result = await chrome.storage.local.get([`page_event_${page}`]);
+    return result[`page_event_${page}`];
+  },
+
+  async savePageEvent(pageEvent, page) {
+    await chrome.storage.local.set({ [`page_event_${page}`]: pageEvent });
+  },
+
+  async addEvent(name, date, page) {
+    if (!name) {
+      alert("请填写定时事件名称！");
+      return;
+    }
+
+    if (!date || date === "00:00:00") {
+      alert("请填写定时事件时间！");
+      return;
+    }
+
+    const milliseconds = TimeUtils.getMilliseconds(
+      ...date.split(":").map(Number)
+    );
+
+    const pageEvent = {
+      name,
+      milliseconds,
+      downTime: date,
+      timer: null,
+      endTime: Date().now,
+      open: false,
+      defaultDate: date,
+    };
+    await this.savePageEvent(pageEvent, page);
+  },
+
+  async deleteEvent(page) {
+    await this.clearTimeout(page);
+    await this.savePageEvent(null, page);
+  },
+
+  async clearTimeout(page) {
+    const pageEvent = await this.getPageEvent(page);
+    clearTimeout(pageEvent.timer);
+    pageEvent.timer = null;
+    await this.savePageEvent(pageEvent, page);
+  },
+
+  // 开始倒计时
+  async start(page) {
+    await this.clearTimeout(page);
+    const pageEvent = await this.getPageEvent(page);
+    if (!pageEvent) return;
+    if (pageEvent.milliseconds === 0) {
+      const milliseconds = TimeUtils.getMilliseconds(
+        ...pageEvent.defaultDate.split(":").map(Number)
+      );
+      pageEvent.milliseconds = milliseconds;
+      pageEvent.downTime = pageEvent.defaultDate;
+    }
+    pageEvent.open = true;
+    // 结束时间戳 = 此刻时间戳 + 剩余的时间
+    pageEvent.endTime = Date.now() + pageEvent.milliseconds + 900;
+    await this.savePageEvent(pageEvent, page);
+    await this.macroTick(page);
+  },
+
+  async startForPageEvent(pageEvent, page) {
+    await this.clearTimeout(page);
+    if (pageEvent.milliseconds === 0) {
+      const milliseconds = TimeUtils.getMilliseconds(
+        ...pageEvent.defaultDate.split(":").map(Number)
+      );
+      pageEvent.milliseconds = milliseconds;
+      pageEvent.downTime = pageEvent.defaultDate;
+      // 结束时间戳 = 此刻时间戳 + 剩余的时间
+      const endTime = Date.now() + pageEvent.milliseconds + 900;
+      pageEvent.endTime = endTime;
+      await this.savePageEvent(pageEvent, page);
+    }
+    await this.savePageEvent(pageEvent, page);
+    await this.macroTick(page);
+  },
+
+  // 暂停倒计时
+  async pause(page) {
+    await this.clearTimeout(page);
+    const pageEvent = await this.getPageEvent(page);
+    if (pageEvent && pageEvent.milliseconds < 1000) {
+      await this.end(page);
+      return;
+    }
+    if (pageEvent) {
+      pageEvent.open = false;
+      await this.savePageEvent(pageEvent, page);
+    }
+  },
+
+  // 定时器
+  async macroTick(page) {
+    // 每隔一定时间，更新一遍定时器的值
+    const timer = setTimeout(async () => {
+      // 获取剩余时间
+      const remain = await this.getRemainTime(page);
+      // 如果时间已到，停止倒计时
+      if (remain === 0) {
+        await this.end(page);
+        // 重设剩余时间
+      } else {
+        const pageEvent = await this.getPageEvent(page);
+        if (pageEvent && !this.isSameSecond(remain, pageEvent.milliseconds)) {
+          pageEvent.milliseconds = remain;
+          pageEvent.downTime = TimeUtils.parseMilliseconds(remain);
+          await this.savePageEvent(pageEvent, page);
+          chrome.runtime.sendMessage({ type: "UPDATE_NEXT", page });
         }
-        
-        const milliseconds = TimeUtils.getMilliseconds(...date.split(':').map(Number))
-        const events = await this.getEvents();
-        const newEvent = {
-            id: Date.now() + 'event',
-            name,
-            milliseconds,
-            downTime: date,
-            show: false,
-            open: false,
-            defaultDate: date
-        };
-        
-        // 最多只能保存3个事件
-        if (events.length > 2) {
-            const head = events.shift()
-            const nextEvent = await this.getNextEvent();
-            if (nextEvent && nextEvent.id === head.id) {
-                await this.saveNextEvent(null);
-            }
-        }
-        events.push(newEvent);
-        await this.saveEvents(events)
-    },
-    
-    async deleteEvent(id) {
-        const events = await this.getEvents();
-        const filteredEvents = events.filter(event => event.id !== id);
-        const nextEvent = await this.getNextEvent();
-        if (nextEvent && nextEvent.id === id) {
-            await this.clearTimeout()
-            await this.saveNextEvent(null);
-        }
-        await this.saveEvents(filteredEvents)
-    },
-    
-    async clearTimeout() {
-        const timer = await this.getTimer()
-        clearTimeout(timer)
-        await this.saveTimer(null)
-    },
-    
-    // 开始倒计时
-    async start(id) {
-        await this.clearTimeout()
-        const events = await this.getEvents();
-        const filteredEvent = events.filter(event => event.id === id)[0];
-        if (!filteredEvent) return
-        // nextEvent 存在，但是肯定点击开始的不是 nextEvent，需要把 前nextEvent 时间重置
-        let nextEvent = await this.getNextEvent();
-        if (nextEvent && nextEvent.id !== id) {
-            const filteredNextEvent = events.filter(event => event.id === nextEvent.id)[0];
-            if (!filteredNextEvent) return
-            const mills = TimeUtils.getMilliseconds(...filteredNextEvent.defaultDate.split(':').map(Number))
-            filteredNextEvent.milliseconds = mills
-            filteredNextEvent.downTime = TimeUtils.parseMilliseconds(mills);
-        }
-        nextEvent = filteredEvent
-        if (nextEvent.milliseconds === 0) {
-            const milliseconds = TimeUtils.getMilliseconds(...nextEvent.defaultDate.split(':').map(Number))
-            nextEvent.milliseconds = milliseconds
-            nextEvent.downTime = nextEvent.defaultDate
-        }
-        for(let i = 0;i < events.length;i++) {
-            if (events[i].id === id) {
-                events[i].show = true
-                events[i].open = true
-            } else {
-                events[i].show = false
-                events[i].open = false
-            }
-        }
-        await this.saveNextEvent(nextEvent);
-        await this.saveEvents(events)
-        // 结束时间戳 = 此刻时间戳 + 剩余的时间
-        const endTime = Date.now() + nextEvent.milliseconds + 900
-        await this.saveEndTime(endTime)
-        await this.macroTick()
-    },
-    
-    async startForNextEvent(nextEvent) {
-        await this.clearTimeout()
-        if (nextEvent.milliseconds === 0) {
-            const milliseconds = TimeUtils.getMilliseconds(...nextEvent.defaultDate.split(':').map(Number))
-            nextEvent.milliseconds = milliseconds
-            nextEvent.downTime = nextEvent.defaultDate
-            // 结束时间戳 = 此刻时间戳 + 剩余的时间
-            const endTime = Date.now() + nextEvent.milliseconds + 900
-            await this.saveEndTime(endTime)
-        }
-        await this.saveNextEvent(nextEvent);
-        await this.macroTick()
-    },
-    
-    // 暂停倒计时
-    async pause(id) {
-        await this.clearTimeout()
-        const nextEvent = await this.getNextEvent();
-        if (nextEvent && nextEvent.milliseconds < 1000) {
-            await this.end()
-            return
-        }
-        const events = await this.getEvents();
-        const index = events.findIndex(event => event.id === id);
-        if (index !== -1) {
-            nextEvent.open = false
-            await this.saveNextEvent(nextEvent);
-            events[index] = nextEvent
-            await this.saveEvents(events)
-        }
-    },
-    
-    // 定时器
-    async macroTick() {
-        // 每隔一定时间，更新一遍定时器的值
-        const timer = setTimeout(async () => {
-            // 获取剩余时间
-            const remain = await this.getRemainTime()
-            // 如果时间已到，停止倒计时
-            if (remain === 0) {
-                await this.end()
-            // 重设剩余时间
-            } else {
-                const nextEvent = await this.getNextEvent();
-                if (nextEvent && !this.isSameSecond(remain, nextEvent.milliseconds)) {
-                    nextEvent.milliseconds = remain
-                    nextEvent.downTime = TimeUtils.parseMilliseconds(remain);
-                    await this.saveNextEvent(nextEvent);
-                    chrome.runtime.sendMessage({ type: 'UPDATE_NEXT' });
-                }
-                await this.macroTick()
-            }
-        }, 30)
-        await this.saveTimer(timer)
-    },
-    // 获取剩余的时间
-    isSameSecond(time1, time2) {
-        return Math.floor(time1 / 1000) === Math.floor(time2 / 1000)
-    },
-    // 获取剩余的时间
-    async getRemainTime() {
-        const endTime = await this.getEndTime()
-        // 取最大值，防止出现小于0的剩余时间值
-        return Math.max(endTime - Date.now(), 0)
-    },
-    // 结束第一轮倒计时
-    async end() {
-        await this.clearTimeout()
-        const events = await this.getEvents();
-        const filteredEvent = events.filter(event => event.open)[0];
-        if (!filteredEvent) return
-        // 暂停
-        // filteredEvent.open = false
-        filteredEvent.milliseconds = 0
-        filteredEvent.downTime = '00:00:00'
-        await this.saveEvents(events)
-        showNotification(filteredEvent.name);
-        this.startForNextEvent(filteredEvent)
-    },
+        await this.macroTick(page);
+      }
+    }, 30);
+    const pageEvent = await this.getPageEvent(page);
+    pageEvent.timer = timer;
+    await this.savePageEvent(pageEvent, page);
+  },
+  // 获取剩余的时间
+  isSameSecond(time1, time2) {
+    return Math.floor(time1 / 1000) === Math.floor(time2 / 1000);
+  },
+  // 获取剩余的时间
+  async getRemainTime(page) {
+    const pageEvent = await this.getPageEvent(page);
+    const endTime = pageEvent.endTime;
+    // 取最大值，防止出现小于0的剩余时间值
+    return Math.max(endTime - Date.now(), 0);
+  },
+  // 结束第一轮倒计时
+  async end(page) {
+    await this.clearTimeout(page);
+    const pageEvent = await this.getPageEvent(page);
+    if (!pageEvent) return;
+    // 暂停
+    // filteredEvent.open = false
+    pageEvent.milliseconds = 0;
+    pageEvent.downTime = "00:00:00";
+    await this.savePageEvent(pageEvent, page);
+    showNotification(pageEvent.name, page);
+    this.startForPageEvent(pageEvent, page);
+  },
 };
--- popup.js
+++ popup.js
@@ -1,130 +1,175 @@
+// 当前 tab 页
+let tabPage = "page1";
+
 // 监听存储变化
 chrome.storage.onChanged.addListener(async (changes, namespace) => {
-    if (namespace === 'local' && changes.events) {
-        eventManager.updateUI(changes.events.newValue);
-    }
+  if (namespace === "local" && changes["page_event_page1"]) {
+    eventManager.displayEvents("page1", changes["page_event_page1"].newValue);
+    eventManager.updatePageEvent("page1", changes["page_event_page1"].newValue);
+  } else if (namespace === "local" && changes["page_event_page2"]) {
+    eventManager.displayEvents("page2", changes["page_event_page2"].newValue);
+    eventManager.updatePageEvent("page2", changes["page_event_page2"].newValue);
+  } else if (namespace === "local" && changes["page_event_page3"]) {
+    eventManager.displayEvents("page3", changes["page_event_page3"].newValue);
+    eventManager.updatePageEvent("page3", changes["page_event_page3"].newValue);
+  }
 });
 
 // 监听来自 background 的消息
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    if (message.type === 'UPDATE_NEXT') {
-        eventManager.updateNextEvent()
-        return true
-    } else if (message.type === 'ADD_SUCCESS') {
-        // 保存用户习惯设置定时时间
-        const eventTime = document.getElementById('eventTime').value
-        await chrome.storage.sync.set({ defaultDate: eventTime });
-        document.getElementById('addEventForm').classList.add('hidden');
-        document.getElementById('addEventBtn').classList.remove('hidden');
-        clearForm();
-        return true
-    }
+  if (message.type === "UPDATE_NEXT") {
+    if (message.page !== tabPage) return;
+    const pageEvent = await eventManager.getPageEvent(message.page);
+    eventManager.updatePageEvent(message.page, pageEvent);
+    return true;
+  } else if (message.type === "ADD_SUCCESS") {
+    const eventTime = document.getElementById("eventTime").value;
+    await chrome.storage.sync.set({ defaultDate: eventTime });
+    // 保存用户习惯设置定时时间
+    if (message.page !== tabPage) return;
+    const pageEvent = await eventManager.getPageEvent(message.page);
+    eventManager.displayEvents(message.page, pageEvent);
+    eventManager.updatePageEvent(message.page, pageEvent);
+    document.getElementById("addEventForm").classList.add("hidden");
+    document.getElementById("addEventBtn").classList.remove("hidden");
+    clearForm();
+    return true;
+  }
 });
 // 事件管理
 const eventManager = {
-    async getEvents() {
-        const result = await chrome.storage.local.get(['events']);
-        return result.events || [];
-    },
-    
-    async getNextEvent() {
-        const result = await chrome.storage.local.get(['nextEvent']);
-        return result.nextEvent;
-    },
-    
-    updateUI(events) {
-        this.displayEvents(events);
-        this.updateNextEvent();
-    },
-    
-    displayEvents(events) {
-        const eventsList = document.getElementById('eventsList');
-        eventsList.innerHTML = '';
-        
-        events.forEach(async (event, index, arr) => {
-                const eventElement = document.createElement('div');
-                eventElement.className = 'event-item';
-                const nextEvent = await this.getNextEvent()
-                if (nextEvent && event.id === nextEvent.id && arr.length !== 1) {
-                    eventElement.className += ' event-item-active'
-                }
-                let toggle = `<button class="start-btn" data-id="${event.id}">开始</button>`
-                if (event.open) {
-                    toggle = `<button class="pause-btn" data-id="${event.id}">暂停</button>`
-                }
-                eventElement.innerHTML = `
-                    <div class="event-name">${event.name}</div>
-                    <div class="event-countdown">${event.defaultDate}</div>
-                    <div class="event-btn">
-                    ${toggle}
-                    <button class="delete-btn" title="删除" data-id="${event.id}">×</button>
-                    </div>
-                `;
-                eventsList.appendChild(eventElement);
-            });
-    },
-    
-    async updateNextEvent() {
-        const nextEventName = document.getElementById('nextEventName');
-        const nextEventCountdown = document.getElementById('nextEventCountdown');
-        
-        const nextEvent = await this.getNextEvent()
-        if (nextEvent) {
-            nextEventName.textContent = nextEvent.name;
-            nextEventCountdown.textContent = nextEvent.downTime;
-        } else {
-            nextEventName.textContent = '暂无事件';
-            nextEventCountdown.textContent = '--';
-        }
-    },
+  async getTabPage() {
+    const result = await chrome.storage.local.get(["tabPage"]);
+    return result.tabPage || "page1";
+  },
+
+  async getPageEvent(page) {
+    const result = await chrome.storage.local.get([`page_event_${page}`]);
+    return result[`page_event_${page}`];
+  },
+
+  async saveTabPage(tabPage) {
+    await chrome.storage.local.set({ tabPage });
+  },
+
+  displayEvents(page, pageEvent) {
+    if (page !== tabPage) return;
+    const eventsList = document.getElementById("eventsList");
+    eventsList.innerHTML = "";
+    if (!pageEvent) return;
+    const eventElement = document.createElement("div");
+    eventElement.className = "event-item";
+    let toggle = `<button class="start-btn">开始</button>`;
+    if (pageEvent.open) {
+      toggle = `<button class="pause-btn">暂停</button>`;
+    }
+    eventElement.innerHTML = `
+            <div class="event-name">${pageEvent.name}</div>
+            <div class="event-countdown">${pageEvent.defaultDate}</div>
+            <div class="event-btn">
+            ${toggle}
+            <button class="delete-btn" title="删除">×</button>
+            </div>
+        `;
+    eventsList.appendChild(eventElement);
+  },
+
+  async updatePageEvent(page, pageEvent) {
+    if (page !== tabPage) return;
+    const pageEventName = document.getElementById("pageEventName");
+    const pageEventCountdown = document.getElementById("pageEventCountdown");
+
+    if (pageEvent) {
+      pageEventName.textContent = pageEvent.name;
+      pageEventCountdown.textContent = pageEvent.downTime;
+    } else {
+      pageEventName.textContent = "暂无事件";
+      pageEventCountdown.textContent = "--";
+    }
+  },
 };
 
 function clearForm() {
-    document.getElementById('eventName').value = '';
-    document.getElementById('eventTime').value = '';
+  document.getElementById("eventName").value = "";
+  document.getElementById("eventTime").value = "";
 }
 
 // 初始化
-document.addEventListener('DOMContentLoaded', async () => {    
-    // 加载事件
-    const events = await eventManager.getEvents();
-    eventManager.updateUI(events);
-    
-    // 设置事件监听
-    document.getElementById('addEventBtn').addEventListener('click', async () => {
-        document.getElementById('addEventForm').classList.remove('hidden');
-        document.getElementById('addEventBtn').classList.add('hidden');
-        const result = await chrome.storage.sync.get(['defaultDate']);
-        const defaultDate = result.defaultDate || "00:01:00";
-        flatpickr("#eventTime", {
-            enableTime: true,
-            enableSeconds: true,
-            noCalendar: true,
-            dateFormat: "H:i:S",
-            time_24hr: true,
-            defaultDate,
-        });
+document.addEventListener("DOMContentLoaded", async () => {
+  // 加载事件
+  tabPage = await eventManager.getTabPage();
+  const pageEvent = await eventManager.getPageEvent(tabPage);
+  eventManager.displayEvents(tabPage, pageEvent);
+  eventManager.updatePageEvent(tabPage, pageEvent);
+
+  // 设置事件监听
+  document.getElementById("addEventBtn").addEventListener("click", async () => {
+    document.getElementById("addEventForm").classList.remove("hidden");
+    document.getElementById("addEventBtn").classList.add("hidden");
+    const result = await chrome.storage.sync.get(["defaultDate"]);
+    const defaultDate = result.defaultDate || "00:01:00";
+    flatpickr("#eventTime", {
+      enableTime: true,
+      enableSeconds: true,
+      noCalendar: true,
+      dateFormat: "H:i:S",
+      time_24hr: true,
+      defaultDate,
     });
-    
-    document.getElementById('cancelAdd').addEventListener('click', () => {
-        document.getElementById('addEventForm').classList.add('hidden');
-        document.getElementById('addEventBtn').classList.remove('hidden');
-        clearForm();
+  });
+
+  document.getElementById("cancelAdd").addEventListener("click", () => {
+    document.getElementById("addEventForm").classList.add("hidden");
+    document.getElementById("addEventBtn").classList.remove("hidden");
+    clearForm();
+  });
+
+  document.getElementById("confirmAdd").addEventListener("click", async () => {
+    const eventName = document.getElementById("eventName").value;
+    const eventTime = document.getElementById("eventTime").value;
+    await chrome.runtime.sendMessage({
+      type: "ADD_EVENT",
+      name: eventName,
+      date: eventTime,
+      page: tabPage,
     });
-    
-    document.getElementById('confirmAdd').addEventListener('click', async () => {
-        const eventName = document.getElementById('eventName').value
-        const eventTime = document.getElementById('eventTime').value
-        await chrome.runtime.sendMessage({ type: 'ADD_EVENT', name: eventName, date: eventTime });
-    });
-    
-    document.getElementById('eventsList').addEventListener('click', async (e) => {
-        if (e.target.classList.contains('delete-btn')) {
-            await chrome.runtime.sendMessage({ type: 'DELETE_EVENT', id: e.target.dataset.id });
-        } else if (e.target.classList.contains('start-btn')) {
-            await chrome.runtime.sendMessage({ type: 'TIME_START', id: e.target.dataset.id });
-        } else if (e.target.classList.contains('pause-btn')) {
-            await chrome.runtime.sendMessage({ type: 'TIME_PAUSE', id: e.target.dataset.id });
-        }
-    });
+  });
+
+  document.getElementById("eventsList").addEventListener("click", async (e) => {
+    if (e.target.classList.contains("delete-btn")) {
+      await chrome.runtime.sendMessage({ type: "DELETE_EVENT", page: tabPage });
+    } else if (e.target.classList.contains("start-btn")) {
+      await chrome.runtime.sendMessage({ type: "TIME_START", page: tabPage });
+    } else if (e.target.classList.contains("pause-btn")) {
+      await chrome.runtime.sendMessage({ type: "TIME_PAUSE", page: tabPage });
+    }
+  });
+
+  // tab 页切换
+  document.getElementById("page1").addEventListener("click", async () => {
+    await switchTab("page1");
+  });
+  // tab 页切换
+  document.getElementById("page2").addEventListener("click", async () => {
+    await switchTab("page2");
+  });
+  // tab 页切换
+  document.getElementById("page3").addEventListener("click", async () => {
+    await switchTab("page3");
+  });
 });
+
+async function switchTab(page) {
+  tabPage = page;
+  await eventManager.saveTabPage(page);
+  const pageEvent = await eventManager.getPageEvent(page);
+  eventManager
+    .updatePageEvent(page, pageEvent)
+    [("page1", "page2", "page3")].forEach((pp) => {
+      if (page === pp) {
+        document.getElementById(pp).classList.add("aside-item-select");
+      } else {
+        document.getElementById(pp).classList.remove("aside-item-select");
+      }
+    });
+}
